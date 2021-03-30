4b239ca4231ce7a985ac4022e5d85866da213872
DaveRoox <davexrusso@gmail.com>
2021-03-30 13:53:31 +0200

    first commit

--- src/test/kotlin/DefaultActuatorTest.kt
+++ src/test/kotlin/DefaultActuatorTest.kt
@@ -11,18 +11,20 @@ class DefaultActuatorTest {
     var maxSize: Int = 0
     lateinit var actuator: Actuator
     lateinit var container: MutableList<Pair<Long, Process>>
+    var timestamp: Long = 0
 
     @Before
     fun prepareTest() {
         maxSize = 15
         actuator = DefaultActuator()
         container = LinkedList()
+        timestamp = 1
     }
 
     @Test
     fun testNoMoreThanMaxSize() {
         (0 until maxSize * 3).forEach { i ->
-            actuator.add(Process(i, Priority.LOW), (i + 1).toLong(), container, maxSize)
+            actuator.add(Process(i, Priority.LOW), timestamp++, container, maxSize)
         }
         assert(container.size <= maxSize) {
             "expected: $container.size <= $maxSize; got: false"
@@ -32,7 +34,7 @@ class DefaultActuatorTest {
     @Test
     fun testAllElementsAreInsertedInOrder() {
         (0 until maxSize).forEach { i ->
-            actuator.add(Process(i, Priority.LOW), (i + 1).toLong(), container, maxSize)
+            actuator.add(Process(i, Priority.LOW), timestamp++, container, maxSize)
         }
         val a = container.dropLast(1)
         val b = container.drop(1)
@@ -44,7 +46,7 @@ class DefaultActuatorTest {
     @Test
     fun testNoMoreProcessAllowedWhenReachingMaxSize() {
         (0 until maxSize + 1).forEach { i ->
-            actuator.add(Process(i, Priority.LOW), (i + 1).toLong(), container, maxSize)
+            actuator.add(Process(i, Priority.LOW), timestamp++, container, maxSize)
         }
         val youngestProcessId = maxSize
         assert(!container.map { it.second.pid }.contains(youngestProcessId)) {
--- src/test/kotlin/FIFOActuatorTest.kt
+++ src/test/kotlin/FIFOActuatorTest.kt
@@ -11,18 +11,20 @@ class FIFOActuatorTest {
     var maxSize: Int = 0
     lateinit var actuator: Actuator
     lateinit var container: MutableList<Pair<Long, Process>>
+    var timestamp: Long = 0
 
     @Before
     fun prepareTest() {
         maxSize = 15
         actuator = FIFOActuator()
         container = LinkedList()
+        timestamp = 1
     }
 
     @Test
     fun testNoMoreThanMaxSize() {
         (0 until maxSize * 3).forEach { i ->
-            actuator.add(Process(i, Priority.LOW), (i + 1).toLong(), container, maxSize)
+            actuator.add(Process(i, Priority.LOW), timestamp++, container, maxSize)
         }
         assert(container.size <= maxSize) {
             "expected: $container.size <= $maxSize; got: false"
@@ -32,7 +34,7 @@ class FIFOActuatorTest {
     @Test
     fun testAllElementsAreInsertedInOrder() {
         (0 until maxSize).forEach { i ->
-            actuator.add(Process(i, Priority.LOW), (i + 1).toLong(), container, maxSize)
+            actuator.add(Process(i, Priority.LOW), timestamp++, container, maxSize)
         }
         val a = container.dropLast(1)
         val b = container.drop(1)
@@ -44,7 +46,7 @@ class FIFOActuatorTest {
     @Test
     fun testTheOldestOneIsRemoved() {
         (0 until maxSize+1).forEach { i ->
-            actuator.add(Process(i, Priority.LOW), (i + 1).toLong(), container, maxSize)
+            actuator.add(Process(i, Priority.LOW), timestamp++, container, maxSize)
         }
         val oldestProcessTimestamp = 1L
         assert(!container.map { it.first }.contains(oldestProcessTimestamp)) {
